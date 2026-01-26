ce6d5ff01715e44d3c9dc200e19b980d22cbc8c2
T.J. Gaffney <gaffney2010@users.noreply.github.com>
2026-01-25 16:45:47 -0800

    wiki.sh takes id argument

--- wiki/wiki.sh
+++ wiki/wiki.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# Check if an argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <Wiki_Title>"
-  exit 1
-fi
+# Parse arguments using getopt
+PARSED=$(getopt -o '' --long id: -n "$0" -- "$@")
+if [ $? -ne 0 ]; then exit 1; fi
+eval set -- "$PARSED"
 
+ID=""
+TITLE=""
+
+while true; do
+    case "$1" in
+        --id) ID="$2"; shift 2 ;;
+        --) shift; break ;;
+        *) echo "Internal error"; exit 1 ;;
+    esac
+done
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 [--id <ID>] <Wiki_Title>"
+    exit 1
+fi
 TITLE="$1"
 # Load configuration
 # 1. System-wide default configuration
@@ -45,6 +59,8 @@ pandoc -f mediawiki -t html "$TMP_DIR/page.wiki" -o "$TMP_DIR/page.html"
 # Rewrite links for valid w3m navigation
 python3 /usr/lib/cablecat-wiki/rewrite_links.py "$TMP_DIR/page.html"
 
-
-
-w3m "$TMP_DIR/page.html"
+if [ -n "$ID" ]; then
+    w3m "$TMP_DIR/page.html#$ID"
+else
+    w3m "$TMP_DIR/page.html"
+fi
